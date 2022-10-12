f6b72ac9abe29df823cde64217f0ed3b295df4b2
Tal <tzilkha@Tals-MacBook-Pro-2.local>
2022-10-12 23:54:18 +0400

    feat: added hardcoded consts and loading fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod constants;
+extern crate ff;
+extern crate rand;
+use ff::*;
+
+#[derive(PrimeField)]
+#[PrimeFieldModulus = "21888242871839275222246405745257275088548364400416034343698204186575808495617"]
+#[PrimeFieldGenerator = "7"]
+pub struct Fr(FrRepr);
+
+// String -> Fr
+pub fn str_to_fr(s: &str) -> Fr {
+    todo!();
+}
+
+// Load constants
+pub fn load_constants() -> Vec<Fr> {
+    let c_str = constants::constants();
+    let mut c: Vec<Fr> = Vec::new();
+    for i in 0..c_str.len() {
+        let b: Fr = str_to_fr(c_str[i]);
+        c.push(b);
+    }
+    return c;
 }
 
 #[cfg(test)]
@@ -7,8 +29,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
